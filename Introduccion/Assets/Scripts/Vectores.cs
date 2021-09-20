755295c8ca10f0cd7a91458e84942ba7d9c29b55
Uriel Haile <perenganito@gmail.com>
2021-09-20 11:07:00 -0500

    Introducción cambios y actualizacion

--- Introduccion/Assets/Scripts/Vectores.cs
+++ Introduccion/Assets/Scripts/Vectores.cs
@@ -4,17 +4,44 @@ using UnityEngine;
 
 public class Vectores : MonoBehaviour
 {
-    public GameObject goal;
-    void Start()
-    {
-
-    }
+    public GameObject goal; // Meta, esfera
+    public float speed;
 
     void Update()
     {
         Vector3 p1 = goal.transform.position;
-        Vector3 p2 = transform.position;
-        Vector3 orientation = new Vector3(p1.x-p2.x, p1.z-p2.y, 0);
-        transform.Translate(0.02f*orientation.normalized);
+        Vector3 p2 = transform.position ;
+        Vector3 dir = p1 - p2;
+
+        Vector3 a = transform.right;
+        Vector3 b = goal.transform.right;
+        Debug.DrawRay(p1,2*b,Color.red);
+        Debug.DrawRay(p2,2*a,Color.red);
+        if(dir.magnitude > 0.5f) 
+        {
+            transform.position = transform.position + speed*dir.normalized;
+        }
+        //float dot = a.x*b.x + a.y*b.y + a.z* b.z;
+        float theta = Mathf.Acos(Vector3.Dot(a,b)/(a.magnitude * b.magnitude));
+        Debug.Log("Ángulo entre los vectores :" + Mathf.Rad2Deg*theta);
+        Vector3 cross = Vector3.Cross(a, b);
+
+        if(Mathf.Rad2Deg * theta > 5) 
+        {
+            float deltad = 40f;
+            if(cross.y > 0) 
+            {
+                transform.Rotate(0, Mathf.Deg2Rad*deltad, 0);
+            }
+            else
+            {
+                transform.Rotate(0, -Mathf.Deg2Rad*deltad, 0);
+            }
+        }
     }
 }
+
+
+
+//        
+
